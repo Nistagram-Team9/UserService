8ca2f8442ec6b60ed8edbb0efb1990974a24b21f
MarinaVojnovic <40699105+MarinaVojnovic@users.noreply.github.com>
2021-05-25 21:55:48 +0200

    Feature branch ex3 (#9)
    * Example for ci pipeline basic.
    
    * testing ci
    
    * ci check

--- user-service/src/main/java/devops/team9/userservice/UserServiceApplication.java
+++ user-service/src/main/java/devops/team9/userservice/UserServiceApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
-		System.out.println("Hello");
+		System.out.println("Hell00aao");
 	}
 
 }
