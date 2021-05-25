8ab6e86a65e92d08d242bc57f278114864433316
MarinaVojnovic <40699105+MarinaVojnovic@users.noreply.github.com>
2021-05-25 23:06:18 +0200

    Feature branch ex8 (#15)
    * Example for ci pipeline basic.
    
    * testing ci
    
    * ci check
    
    * ci 4
    
    * ci 5
    
    * ci 6
    
    * ci 7
    
    * ci 9
    
    * ci 10

--- user-service/src/main/java/devops/team9/userservice/UserServiceApplication.java
+++ user-service/src/main/java/devops/team9/userservice/UserServiceApplication.java
@@ -11,7 +11,13 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 		System.out.println("Hell00aaoa");
+
+		System.out.println("Hell00aaoa11");
+		System.out.println("Hell00aaoa11");
+		System.out.println("Hell00aaoa1001");
+
 		System.out.println("Hell00aaoa1133eee");
+
 
 
 
